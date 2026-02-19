38a92502de53a90356e0beef92743853d7c56266
Rabeea Shahid <134997377+Rabeea-S@users.noreply.github.com>
2026-02-19 00:35:13 -0700

    added q1

---
+++ question1/target/debug/deps/question1.d
@@ -0,0 +1,5 @@
+C:\Users\rabee\421\assignment5\question1\target\debug\deps\question1.d: src\main.rs
+
+C:\Users\rabee\421\assignment5\question1\target\debug\deps\question1.exe: src\main.rs
+
+src\main.rs:
---
+++ question1/target/debug/question1.d
@@ -0,0 +1,1 @@
+C:\Users\rabee\421\assignment5\question1\target\debug\question1.exe: C:\Users\rabee\421\assignment5\question1\src\main.rs
